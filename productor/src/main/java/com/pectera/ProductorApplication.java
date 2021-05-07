a8d92e6ec6c40889eedb18571ea8649f3c0ee5f1
james <1406772393@qq.com>
2021-05-07 09:15:01 +0800

    project init

---
+++ productor/src/main/java/com/pectera/ProductorApplication.java
@@ -0,0 +1,16 @@
+package com.pectera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author James
+ * @date 2021/5/6
+ * @decription
+ */
+@SpringBootApplication
+public class ProductorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProductorApplication.class,args);
+	}
+}
